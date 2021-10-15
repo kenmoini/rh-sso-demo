17a56ab332705c90553f3e5f39a06084abca668d
Ken Moini <ken@kenmoini.com>
2021-10-14 22:03:45 -0400

    idk just because

--- container-image/themes/my-purrina/login/resources/js/script.js
+++ container-image/themes/my-purrina/login/resources/js/script.js
@@ -1,3 +1,6 @@
 window.onload = function() {
   document.getElementById("kc-page-title").innerHTML = "Wag In &#128062;";
 }
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("kc-page-title").innerHTML = "Wag In &#128062;";
+});
